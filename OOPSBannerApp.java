3bcba98ef86e07171a84cc5199922f88f2584f41
Abhishekkr2101 <ak1120@srmist.edu.in>
2026-03-10 01:42:03 +0530

    Add files via upload

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,78 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * OOPSBannerApp
+ * UC8 - Use Map for Character Patterns and Render via Function
+ *
+ * @author Abhishek
+ * @version 8.0
+ */
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        Map<Character, String[]> patternMap = buildPatternMap();
+
+        renderBanner("OOPS", patternMap);
+    }
+
+    // Function to build character patterns
+    public static Map<Character, String[]> buildPatternMap() {
+
+        Map<Character, String[]> map = new HashMap<>();
+
+        map.put('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        map.put('P', new String[]{
+                "*****  ",
+                "*    * ",
+                "*    * ",
+                "*****  ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
+
+        map.put('S', new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        });
+
+        return map;
+    }
+
+    // Function to render banner
+    public static void renderBanner(String word, Map<Character, String[]> patternMap) {
+
+        for (int row = 0; row < 7; row++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (char c : word.toCharArray()) {
+
+                String[] pattern = patternMap.get(c);
+
+                if (pattern != null) {
+                    line.append(pattern[row]).append("  ");
+                }
+            }
+
+            System.out.println(line.toString());
+        }
     }
 }
