54531a891a1dc566623f6dbeb71c627ff7c3b8ae
Abhishekkr2101 <ak1120@srmist.edu.in>
2026-03-10 01:39:22 +0530

    Add files via upload

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,52 @@
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        String[] O = getO();
+        String[] P = getP();
+        String[] S = getS();
+
+        for (int i = 0; i < O.length; i++) {
+            System.out.println(O[i] + " " + O[i] + " " + P[i] + " " + S[i]);
+        }
+    }
+
+    // Letter O
+    public static String[] getO() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+    // Letter P
+    public static String[] getP() {
+        return new String[]{
+            "***** ",
+            "*    *",
+            "*    *",
+            "***** ",
+            "*     ",
+            "*     ",
+            "*     "
+        };
+    }
+
+    // Letter S
+    public static String[] getS() {
+        return new String[]{
+            " *****",
+            "*     ",
+            "*     ",
+            " *****",
+            "     *",
+            "     *",
+            "***** "
+        };
     }
 }
